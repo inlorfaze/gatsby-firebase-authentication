d764c50b16c67bcae93a97a972c855beddd37ad6
Finley Chen <finleyjchen@gmail.com>
2018-12-03 14:53:14 -0800

    about page added

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from "../components/layout";
+
+const About = () => (
+  <div>
+    <h1>About polytalent</h1>
+  </div>
+)
+
+
+
+export default () => {
+  <Layout>
+    <About />
+  </Layout>
+}
